85cb50ddd2e856b7d6f9103aab92f5c8a17d4e09
ArtemIvanovski <a.ivanovskiy03@mail.ru>
2022-02-24 14:31:34 +0300

    first commit

--- src/Task6.java
+++ src/Task6.java
@@ -7,6 +7,7 @@ public class Task6 {
         int number = in.nextInt();
         System.out.println("Last number 7 in "+number+"?");
         System.out.println(check(number));
+
     }
     static boolean check(int a){
         return (a%10==7);
