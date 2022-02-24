e47c8673a8e080b0873742816f2c0a4098f7e624
ArtemIvanovski <a.ivanovskiy03@mail.ru>
2022-02-24 14:29:19 +0300

    first commit

---
+++ src/Task6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input number: ");
+        int number = in.nextInt();
+        System.out.println("Last number 7 in "+number+"?");
+        System.out.println(check(number));
+    }
+    static boolean check(int a){
+        return (a%10==7);
+    }
+}
