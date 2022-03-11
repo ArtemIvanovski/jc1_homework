756bbc822fed2942f0f4ec26133f6cb060645d51
ArtemIvanovski <a.ivanovskiy03@mail.ru>
2022-03-11 19:39:45 +0300

    first commit

---
+++ src/Task13/FindFac.java
@@ -0,0 +1,25 @@
+package Task13;
+
+import java.math.BigInteger;
+
+public class FindFac {
+
+    public BigInteger FindFac(int number){
+        long ans;
+        if (number<2)
+            return BigInteger.valueOf(1);
+        else
+            return Fac(number);
+    }
+
+
+    private BigInteger Fac(int num){
+        BigInteger ans = BigInteger.valueOf(1);
+
+        while (num>0){
+            ans = ans.multiply(BigInteger.valueOf(num));
+            num--;
+        }
+        return ans;
+    }
+}
