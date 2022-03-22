80d0d7bd96cd2b05a3f2716a8cadf7cab5169467
ArtemIvanovski <a.ivanovskiy03@mail.ru>
2022-03-22 19:13:07 +0300

    first commitf

--- src/Task23.java
+++ src/Task23.java
@@ -9,6 +9,8 @@ public class Task23 {
                 array[array.length-1-i][j]=1;
             }
         }
+
+
         for (int i=0;i< array.length;i++){
             for (int j=0;j< array.length;j++)
                 System.out.print(array[i][j]+" ");
