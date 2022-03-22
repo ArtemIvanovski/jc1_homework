ae894479dff5b97a1e1d8792eca6979b062bab89
ArtemIvanovski <a.ivanovskiy03@mail.ru>
2022-03-22 19:11:28 +0300

    first commitf

--- src/Task23.java
+++ src/Task23.java
@@ -1,11 +1,18 @@
 public class Task23 {
     public static void main(String[] args) {
         int[][] array = new int[5][5];
-        int k=0;
-        for (int i=0;i<5;i++){
-            k=i;
-            for (int j=0;j<5;j++)
-                array[i][j+k]=1;
+        for (int i=0;i<array.length/2+1;i++){
+            for (int j=i;j< array.length-i;j++){
+                array[i][j]=0;
+                array[array.length-1-i][j]=0;
+                array[i][j]=1;
+                array[array.length-1-i][j]=1;
+            }
+        }
+        for (int i=0;i< array.length;i++){
+            for (int j=0;j< array.length;j++)
+                System.out.print(array[i][j]+" ");
+            System.out.println();
         }
     }
 }
