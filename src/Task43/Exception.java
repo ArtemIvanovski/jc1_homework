21406e55ed7814d66b8851e9eceb6b783f5e6d7b
ArtemIvanovski <a.ivanovskiy03@mail.ru>
2022-04-19 14:25:38 +0300

    first commit

--- src/Task43/Exception.java
+++ src/Task43/Exception.java
@@ -1,9 +1,25 @@
 package Task43;
 
+import Task44.MyException;
+
 public class Exception {
 
     public double divide(Integer a, Integer b){
         return a/b;
     }
 
+    public void makeException(String message) throws MyException {
+        try {
+            createMyException(message);
+        } catch (MyException e) {
+            System.out.println("Thi is higher level exception");
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void createMyException(String message) throws MyException {
+        throw new MyException(message);
+    }
 }
---
+++ src/Task45/Main.java
@@ -0,0 +1,10 @@
+package Task45;
+
+import Task43.Exception;
+import Task44.MyException;
+
+public class Main {
+    public static void main(String[] args) throws MyException {
+        new Exception().makeException("");
+    }
+}
