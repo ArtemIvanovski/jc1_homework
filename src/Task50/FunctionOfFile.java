5f3acd36eab1be6fdb25e914c3a10403bb74a86e
ArtemIvanovski <a.ivanovskiy03@mail.ru>
2022-05-03 11:50:58 +0300

    first commit

--- src/Task50/FunctionOfFile.java
+++ src/Task50/FunctionOfFile.java
@@ -25,7 +25,6 @@ public class FunctionOfFile {
         try {
             FileWriter writer = new FileWriter(pathToFile, false);
             for (T element : list) {
-                System.out.println(element);
                 writer.write(element + symbol);
             }
             writer.close();
